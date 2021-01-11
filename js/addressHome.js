8d2a77e133f6e2d6bd830fbc676dcd0968305126
vijay300 <gnvijayk@gmail.com>
2021-01-11 14:32:38 +0530

    [Vijay] Refactored: js to take the data form local storage instead of dummy json object

--- js/addressHome.js
+++ js/addressHome.js
@@ -1,13 +1,20 @@
+let addressList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    addressList = getAddressDataFromStorage();
+    //document.querySelector("person-count").textContent = addressList.length;
     createInnerHtml();
 });
 
+const getAddressDataFromStorage = () => {
+    return localStorage.getItem('AddressBook') ? JSON.parse(localStorage.getItem('AddressBook')) : [];
+}
+
 const createInnerHtml = () => {
     const headerHtml = "<th>Full Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th></th>"
-    let innerHtml2 = `${headerHtml}`;
-    let addressList = createAddressBookJSON();
+    if (addressList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
     for (const addressBookData of addressList) {
-        innerHtml = `${innerHtml2}
+        innerHtml = `${innerHtml}
         <tr>
             <td>${addressBookData._name}</td>
             <td>${addressBookData._address}</td>
@@ -20,31 +27,6 @@ const createInnerHtml = () => {
                 <img name="${addressBookData._id}" onclick="update(this) alt="edit" src="../assets/create-black-18dp.svg">
             </td>
         </tr>
-        `;
-    }
+    `;}
     document.querySelector('#table-display').innerHTML = innerHtml;
-}
-
-const createAddressBookJSON = () => {
-    let localAddress = [
-        {
-            _name = 'Vijay',
-            _address = '31-36-110 Rajeev Nagar',
-            _city = 'Visakhapatnam',
-            _state = 'Andhra Pradesh',
-            _zipCode = '530046',
-            _phoneNumber = '91 8977890300',
-            _id = new Date().getTime
-        },
-        {
-            _name = 'Kumar',
-            _address = '5-4-2 City Nagar',
-            _city = 'Pune',
-            _state = 'Maharastra',
-            _zipCode = '300291',
-            _phoneNumber = '91 8786892320',
-            _id = new Date().getTime
-        }
-    ];
-    return localAddress;
 }
